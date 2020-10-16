5469c45804727cce73a85a841a84744445c771e0
Kenneth Duong <kduong125@gmail.com>
2020-10-16 15:32:30 -0700

    Initial Commit

---
+++ SwimTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwimTracker
+//
+//  Created by Kenneth Duong on 10/16/20.
+//  Copyright © 2020 Kenneth Duong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
